9965e68e54ce46b13f06826349769a0e647fc0fd
jeiisonbf <jeiisonbf@gmail.com>
2021-02-16 19:31:40 -0500

    algo

---
+++ conexion/Conexion.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace Iris.conexion
+{
+    public class Conexion
+    {
+        private SqlConnection con;
+        public static Conexion cnn;
+
+        public Conexion()
+        {
+            Console.Write("creando instancia de conexion");
+            String cadenaConexion = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+            con = new SqlConnection(cadenaConexion);
+        }
+
+        public SqlConnection getConexion()
+        {
+            return con;
+        }
+
+        public void cerrarConexion()
+        {
+            con.Close();
+            cnn = null;
+        }
+
+        public void abrirConexion()
+        {
+            con.Open();
+        }
+
+    
+    }
+}
